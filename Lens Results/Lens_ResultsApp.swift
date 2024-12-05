f5361ac33e5e6a8d371f20878ba886cfbc8c38b8
D-H-O-R-A <diegoantunes2301@gmail.com>
2024-12-04 22:33:43 -0500

    Initial Commit

---
+++ Lens Results/Lens_ResultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lens_ResultsApp.swift
+//  Lens Results
+//
+//  Created by user268572 on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Lens_ResultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
