5ffa46be837ffe02db68231a55016d89140347ec
D-H-O-R-A <diegoantunes2301@gmail.com>
2024-12-15 11:11:40 -0500

    Initial Commit

---
+++ AR Box DetectionTests/AR_Box_DetectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_Box_DetectionTests.swift
+//  AR Box DetectionTests
+//
+//  Created by user268572 on 12/15/24.
+//
+
+import Testing
+@testable import AR_Box_Detection
+
+struct AR_Box_DetectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
